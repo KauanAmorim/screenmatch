20cb1f54d892d06cee3a37bf88f496e2b18f1ac2
KauanAmorim <kauanamorim88@gmail.com>
2025-06-16 18:03:26 -0300

    update seriesData

--- src/main/java/br/com/kauanamorim/screenmatch/model/SeriesData.java
+++ src/main/java/br/com/kauanamorim/screenmatch/model/SeriesData.java
@@ -7,5 +7,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record SeriesData(@JsonAlias("Title") String title,
                          @JsonAlias("totalSeasons") Integer totalSeasons,
                          @JsonAlias("imdbRating") String rating,
-                         @JsonAlias("Released") String releaseDate) {
+                         @JsonAlias("Released") String releaseDate,
+                         @JsonAlias("Genre") String genre,
+                         @JsonAlias("Actors") String actors,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Plot") String plot) {
 }
